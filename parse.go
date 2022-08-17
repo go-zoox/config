159e708fd9aab860b97ec1e7937a069c30b36c91
Zero <tobewhatwewant@outlook.com>
2022-08-17 07:27:36 +0000

    fix: parse config

--- parse.go
+++ parse.go
@@ -1,8 +1,19 @@
 package config
 
-import "github.com/go-zoox/encoding/yaml"
+import (
+	"github.com/go-zoox/encoding/yaml"
+	"github.com/go-zoox/tag"
+	"github.com/go-zoox/tag/datasource"
+)
 
 // Parse parses the config from the given raw config.
 func Parse(data []byte, config interface{}) error {
-	return yaml.Decode(data, config)
+	configYaml := make(map[string]any)
+	if err := yaml.Decode(data, &configYaml); err != nil {
+		return err
+	}
+
+	tg := tag.New("config", datasource.NewMapDataSource(configYaml))
+
+	return tg.Decode(config)
 }
