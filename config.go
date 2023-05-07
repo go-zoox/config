7a3147d344e59599f39f611ccca3feb44c34732e
whatwewant <tobewhatwewant@outlook.com>
2023-05-07 23:40:37 +0800

    chore: config load priority

--- config.go
+++ config.go
@@ -22,49 +22,63 @@ type LoadOptions struct {
 	// options: YAML | JSON | TOML | INI | HOST, default: YAML
 	Type string
 
-	// Unique Name for the config file, default: "", and type is YAML
+	// Unique AppName for the config file, default: ""
+	AppName string
+
+	// Config name, default: config.yml, and type is YAML
 	Name string
 }
 
 // Load loads the config from the given file path.
 // If the file path is empty, it will load the config from the default file path.
-// Default file path is ${PWD}/.{NAME}.yml > ${PWD}/.config.yml > /etc/{NAME}/config.yml (user is root) | $HOME/.config/{NAME}/config.yml.
+// Default file path is
+//
+//	 custom file path
+//		> ${PWD}/.{APP_NAME}.{NAME}.yml
+//		/etc/{APP_NAME}/{NAME}.yml (user is root) | $HOME/.config/{APP_NAME}/{NAME}.yml.
+//		> ${PWD}/.{APP_NAME}.yml
+//		> /etc/{APP_NAME}/config.yml (user is root) | $HOME/.config/{APP_NAME}/config.yml.
+//		> ${PWD}/.config.yml
 func Load(config any, options ...*LoadOptions) error {
 	fileType := DefaultFileType
 	filepathX := fs.JoinCurrentDir(".config.yml")
 
-	if len(options) > 0 {
+	if len(options) > 0 && options[0] != nil {
 		optionsX := options[0]
-
-		if optionsX.Type != "" {
-			fileType = optionsX.Type
-		}
 
 		if optionsX.FilePath != "" {
 			filepathX = optionsX.FilePath
-			ext := fs.ExtName(filepathX)
-
-			switch ext {
-			case "", ".yml", ".yaml":
-				fileType = "YAML"
-			case ".json":
-				fileType = "JSON"
-			case ".toml":
-				fileType = "TOML"
-			case ".ini":
-				fileType = "INI"
-			case ".host":
-				fileType = "HOST"
-			default:
-				return fmt.Errorf("unsupported file type: %s", ext)
-			}
-		} else if optionsX.Name != "" {
-			// ${PWD}/.{NAME}.yml > ${PWD}/.{NAME}.yml > /etc/{NAME}/config.yml (user is root) | $HOME/.config/{NAME}/config.yml
-			if fs.IsExist(fs.JoinCurrentDir("." + optionsX.Name + ".yml")) {
-				filepathX = fs.JoinCurrentDir("." + optionsX.Name + ".yml")
+		} else if optionsX.AppName != "" {
+			if optionsX.Name != "" {
+				if fs.IsExist(fs.JoinCurrentDir(fmt.Sprintf(".%s.%s.yml", optionsX.AppName, optionsX.Name))) {
+					filepathX = fs.JoinCurrentDir(fmt.Sprintf(".%s.%s.yml", optionsX.AppName, optionsX.Name))
+				} else {
+					filepathX = fs.JoinConfigDir(optionsX.AppName, optionsX.Name)
+				}
 			} else {
-				filepathX = fs.JoinConfigDir(optionsX.Name)
+				// ${PWD}/.{APP_NAME}.yml > ${PWD}/.{APP_NAME}.yml > /etc/{APP_NAME}/config.yml (user is root) | $HOME/.config/{APP_NAME}/config.yml
+				if fs.IsExist(fs.JoinCurrentDir(fmt.Sprintf(".%s.yml", optionsX.AppName))) {
+					filepathX = fs.JoinCurrentDir(fmt.Sprintf(".%s.yml", optionsX.AppName))
+				} else {
+					filepathX = fs.JoinConfigDir(optionsX.AppName)
+				}
 			}
+		}
+
+		ext := fs.ExtName(filepathX)
+		switch ext {
+		case "", ".yml", ".yaml":
+			fileType = "YAML"
+		case ".json":
+			fileType = "JSON"
+		case ".toml":
+			fileType = "TOML"
+		case ".ini":
+			fileType = "INI"
+		case ".host":
+			fileType = "HOST"
+		default:
+			return fmt.Errorf("unsupported file type: %s", ext)
 		}
 	}
 
